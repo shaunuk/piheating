ffcfd27dc31bb1d26a8d0f52fe53d9ba06505c7c
shaunuk <shaun@pppoe.co.uk>
2014-11-17 14:52:21 +0000

    Only logs on temp change, not all the time. Check temp every 5 seconds.

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var gpio = require("pi-gpio");
 var fs = require('fs');
 var mysql = require('mysql');
+var static = require('node-static');
 
 global.pin = -1;
+global.temp = 0;
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(80);
+
+
+
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
@@ -13,21 +31,24 @@ var connection = mysql.createConnection({
 function getTemp() {
   fs.readFile('/sys/bus/w1/devices/w1_bus_master1/28-000004f90d18/w1_slave', 'utf8', function (err, data) {
     if (err) throw err;
-    temp = data.slice(data.search('t=')+2);
-    console.log('%s',temp);
-    if (temp > 22000) {
-      relay(11,1);
-    } else { 
-      relay(11,0);
+    temp = parseFloat((data.slice(data.search('t=')+2)/1000).toFixed(1));
+    console.log('Current Temperature %s last %s',temp,global.lastTemp);
+    if (global.lastTemp != temp) { //only do logic and save if the tewmp has changed
+      if (temp > 19) {
+        relay(11,1);
+      } else { 
+        relay(11,0);
+      }
+      connection.query('insert into log values ("0",now(),"T","'+temp+'")', function(err, rows) {
+      console.log('written');
+        if (err) {
+          console.error('error connecting: ' + err.stack);
+          return;
+        }
+        global.lastTemp = temp; //set global for next time
+        // connected! (unless `err` is set)
+      });
     }
-    connection.query('insert into log values ("0",now(),"T","'+temp+'")', function(err, rows) {
-if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-      // connected! (unless `err` is set)
-     console.log('written');
-    });
   });
 }
 
@@ -47,7 +68,7 @@ function relay(pin,position) {
 
 setInterval(function(){
   getTemp();
-}, 60000);
+}, 5000);
 
 console.log('Running');
 
